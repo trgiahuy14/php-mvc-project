1c054f796e8537dd7adab19f6f90eb309d292af7
trgiahuy14 <trgiahuy14@gmail.com>
2025-11-08 15:00:01 +0700

    refactor(posts): improve add post code structure

--- app/Views/layouts-part/posts/add.php
+++ app/Views/layouts-part/posts/add.php
@@ -2,7 +2,7 @@
 if (!defined('_TRGIAHUY')) {
     die('Truy cập không hợp lệ');
 }
-$data = ['title' => 'Thêm mới khóa học'];
+$data = ['title' => 'Thêm bài viết'];
 layout('header', $data);
 layout('sidebar');
 
@@ -13,96 +13,100 @@ $errorsArr = getSessionFlash('errors');
 ?>
 
 <div class="container add-user">
-    <h2>Thêm mới khóa học</h2>
+    <h2>Thêm bài viết</h2>
     <hr>
     <?php
     if (!empty($msg) && !empty($msg_type)) {
         getMsg($msg, $msg_type);
     }
     ?>
+    <!-- Post creation form -->
     <form action="" method="post" enctype="multipart/form-data">
         <div class="row">
-
             <!-- Post title -->
             <div class="col-12 pb-3">
                 <label for="title">Tiêu đề bài viết</label>
-                <input id="title" name="title" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'title');
-                            }  ?>"
-                    placeholder="Nhập tiêu đề">
-                <?php
-                if (!empty($errorsArr)) {
-                    echo formError($errorsArr, 'title');
-                } ?>
+                <input
+                    id="title"
+                    name="title"
+                    type="text"
+                    class="form-control"
+                    placeholder="Nhập tiêu đề"
+                    value="<?= (!empty($oldData)) ? oldData($oldData, 'title') : null ?>">
+                <!-- Error -->
+                <?= (!empty($errorsArr)) ? formError($errorsArr, 'title') : null ?>
             </div>
 
             <!-- Post content -->
             <div class="col-12 pb-3">
                 <label for="content">Nội dung bài viết</label>
-                <textarea id="content" name="content" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'content');
-                            } ?>"
-                    placeholder="Nội dung"></textarea>
-                <?php
-                if (!empty($errorsArr)) {
-                    echo formError($errorsArr, 'content');
-                } ?>
+                <textarea
+                    id="content"
+                    name="content"
+                    class="form-control"
+                    placeholder="Nội dung"><?= (!empty($oldData)) ? oldData($oldData, 'content') : null ?></textarea>
+                <!-- Error -->
+                <?= (!empty($errorsArr)) ? formError($errorsArr, 'title') : null ?>
             </div>
 
             <!-- Post tags-->
             <div class="col-12 pb-3">
                 <label for="tags">Tags</label>
-                <textarea id="tags" name="tags" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'tags');
-                            }  ?>"
-                    placeholder="Nhập thẻ"></textarea>
+                <textarea
+                    id="tags"
+                    name="tags"
+                    class="form-control"
+                    placeholder="Nhập thẻ"><?= (!empty($oldData)) ? oldData($oldData, 'minutes_read') : null ?></textarea>
             </div>
 
             <!-- Minutes read -->
             <div class="col-3 pb-3">
                 <label for="minutes_read">Thời gian đọc</label>
-                <input id="minutes_read" name="minutes_read" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'minutes_read');
-                            }  ?>"
-                    placeholder="Thời gian đọc">
+                <input
+                    id="minutes_read"
+                    name="minutes_read"
+                    type="text"
+                    class="form-control"
+                    placeholder="Thời gian đọc"
+                    value="<?= (!empty($oldData)) ? oldData($oldData, 'minutes_read') : null ?>">
             </div>
 
 
             <!-- Views -->
             <div class="col-3 pb-3">
                 <label for="views">Views</label>
-                <input id="views" name="views" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'views');
-                            }  ?>"
-                    placeholder="Nhập số lượt xem">
+                <input
+                    id="views"
+                    name="views"
+                    type="text"
+                    class="form-control"
+                    placeholder="Nhập số lượt xem"
+                    value="<?= (!empty($oldData)) ? oldData($oldData, 'views') : null ?>">
             </div>
 
             <!-- Comments -->
             <div class="col-3 pb-3">
                 <label for="comments">Bình luận</label>
-                <input id="comments" name="comments" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'comments');
-                            }  ?>"
-                    placeholder="Nhập số lượt bình luận">
+                <input
+                    id="comments"
+                    name="comments"
+                    type="text"
+                    class="form-control"
+                    placeholder="Nhập số lượt bình luận"
+                    value="<?= (!empty($oldData)) ? oldData($oldData, 'views') : null ?>">
             </div>
 
             <!-- Shares -->
             <div class="col-3 pb-3">
                 <label for="shares">Lượt chia sẻ</label>
-                <input id="shares" name="shares" type="text" class="form-control"
-                    value="<?php if (!empty($oldData)) {
-                                echo oldData($oldData, 'shares');
-                            }  ?>"
-                    placeholder="Nhập số lượt chia sẻ">
+                <input
+                    id="shares"
+                    name="shares"
+                    type="text"
+                    class="form-control"
+                    placeholder="Nhập số lượt chia sẻ"
+                    value="<?= (!empty($oldData)) ? oldData($oldData, 'views') : null ?>">
             </div>
-
-
         </div>
         <button type="submit" class="btn btn-success">Xác nhận</button>
     </form>
