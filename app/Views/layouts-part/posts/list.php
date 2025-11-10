8b59cf49a5dcbdd26e679bee4b8fd9bf5554dc1f
trgiahuy14 <trgiahuy14@gmail.com>
2025-11-11 01:01:01 +0700

    feat(posts): improve listing table UI and new columns
    - Add columns: author, comments, shares
    - Apply Bootstrap styling to table

--- app/Views/layouts-part/posts/list.php
+++ app/Views/layouts-part/posts/list.php
@@ -26,7 +26,7 @@ $msg_type = getSessionFlash('msg_type');
             <div class="row">
                 <div class="col-7">
                     <input class="form-control" type="text" value="<?= (!empty($keyword)) ? $keyword : false ?>"
-                        name="keyword" placeholder="Nhập thông tin tìm kiếm...">
+                        name="keyword" placeholder="Nhập tiêu đề bài viết để tìm kiếm...">
                 </div>
                 <!-- Search button -->
                 <div class="col-2">
@@ -36,31 +36,71 @@ $msg_type = getSessionFlash('msg_type');
         </form>
 
         <!-- Posts listing table -->
-        <table class="table table-bordered text-center">
+        <table class="table table-striped table-hover align-middle">
             <thead>
                 <tr>
-                    <th scope="col">STT</th>
-                    <th scope="col">Tiêu đề</th>
-                    <th scope="col">Nội dung</th>
-                    <th scope="col">Ngày viết</th>
-                    <th scope="col">Sửa</th>
-                    <th scope="col">Xóa</th>
+                    <th style="width:60px" class="text-center">STT</th>
+                    <th style="width:280px">Tiêu đề</th>
+                    <th style="width:140px">Tác giả</th>
+                    <th style="width:180px">Tags</th>
+                    <th style="width:100px" class="text-center">Lượt xem</th>
+                    <th style="width:100px" class="text-center">Bình luận</th>
+                    <th style="width:120px" class="text-center">Lượt chia sẻ</th>
+                    <th style="width:120px" class="text-center">Cập nhật</th>
+                    <th style="width:110px" class="text-center">Action</th>
                 </tr>
             </thead>
             <tbody>
-                <?php $count = $offset + 1; ?>
-                <?php foreach ($postDetail as $key => $item): ?>
+                <?php $count = $offset + 1;
+                foreach ($postDetail as $item): ?>
+                    <?php $date = $item['updated_at'] ?: $item['created_at']; ?>
                     <tr>
-                        <th scope="row"><?= $count++  ?></th>
-                        <td><?= $item['title']; ?></td>
-                        <td><?= $item['content']; ?></td>
-                        <td><?= $item['created_at']; ?></td>
-                        <td><a href="<?= BASE_URL; ?>/posts/edit?id=<?= $item['id']; ?>" class="btn btn-warning"><i class="fa-solid fa-pencil"></i></a></td>
-                        <td><a href="<?= BASE_URL; ?>/posts/delete?id=<?= $item['id']; ?>" onclick="return confirm('Bạn có chắc chắn muốn xóa không')" class=" btn btn-danger"><i class="fa-solid fa-trash"></i></a></td>
+                        <th class="text-center" scope="row"><?= $count++ ?></th>
+
+                        <!-- Title -->
+                        <td class="text-truncate" style="max-width:260px"
+                            title="<?= htmlspecialchars($item['content'] ?? $item['title']) ?>">
+                            <a href="<?= BASE_URL ?>/posts/edit?id=<?= $item['id'] ?>" class="link-dark text-decoration-none">
+                                <?= htmlspecialchars($item['title']) ?>
+                            </a>
+                        </td>
+
+                        <!-- Author -->
+                        <td class="text-nowrap"><?= htmlspecialchars($item['author']) ?></td>
+
+                        <!-- Tags -->
+                        <td class="text-truncate" style="max-width:200px"
+                            title="<?= htmlspecialchars($item['tags']) ?>">
+                            <?= htmlspecialchars($item['tags']) ?>
+                        </td>
+
+                        <!-- Views -->
+                        <td class="text-center"><?= shortNumber($item['views']) ?></td>
+
+                        <!-- Comments -->
+                        <td class="text-center"><?= shortNumber($item['comments']) ?></td>
+
+                        <!-- Shares -->
+                        <td class="text-center"><?= shortNumber($item['shares']) ?></td>
+
+                        <!-- Date modified -->
+                        <td class="text-center text-nowrap"><?= date('d-m-Y', strtotime($date)) ?></td>
+
+                        <td class="text-center">
+                            <a href="<?= BASE_URL ?>/posts/edit?id=<?= $item['id'] ?>" class="btn btn-warning btn-sm">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            <a href="<?= BASE_URL ?>/posts/delete?id=<?= $item['id'] ?>"
+                                class="btn btn-danger btn-sm"
+                                onclick="return confirm('Bạn có chắc chắn muốn xoá không?')">
+                                <i class="fa fa-trash"></i>
+                            </a>
+                        </td>
                     </tr>
                 <?php endforeach; ?>
             </tbody>
         </table>
+
 
         <!-- Pagination -->
         <nav aria-label="Page navigation example">
